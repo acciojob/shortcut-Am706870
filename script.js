01d2b0874f788deba95ea7b7d82505ed11ae9861
Am706870 <88397078+Am706870@users.noreply.github.com>
2024-12-20 14:17:49 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,8 +1,17 @@
 function shortcut(s1, s2) {
+	if (s1 === "" || s2 === "") {
+    return "";
+  }
+  
+  // Return the first character of both strings concatenated
+  return s1[0] + s2[0];
+	
+	
+	
   // your code here
 }
 
-// Do not change the code below.
+// // Do not change the code below.
 const s1 = prompt("Enter s1:");
 const s2 = prompt("Enter s2:");
 alert(shortcut(s1, s2));
